2a1e714d89a3465b46f9e90a8bc30192ba0e8f04
“Aaron <barfootaaron@yahoo.com>
2017-12-06 20:45:27 -0600

    fix models in admin

--- website/admin.py
+++ website/admin.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
 
-from .models import Trip, TripType, PaymentType, Order
+from .models import Trip, TripType, PaymentType, Order, Customer, TripOrder, TripReview, WishList
 
 # Register your models here.
 admin.site.register(Trip)
 admin.site.register(TripType)
 admin.site.register(PaymentType)
 admin.site.register(Order)
+admin.site.register(Customer)
+admin.site.register(TripOrder)
+admin.site.register(TripReview)
+admin.site.register(WishList)
 
